302c2bb9da7a55c239734279a709c5f7799d8752
Sachin-fsd <sachineducational555@gmail.com>
2025-07-18 12:15:55 +0530

    bug fixes.

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.scss";
 import Featured from "../../components/featured/Featured";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
 import Slide from "../../components/slide/Slide";
-import CatCard from "../../components/catCard/CatCard.jsx";
+import CatCard from "../../components/catCard/CatCard";
 import ProjectCard from "../../components/projectCard/ProjectCard";
 import { cards, projects } from "../../data";
 import Check from "lucide-react/dist/esm/icons/check";
